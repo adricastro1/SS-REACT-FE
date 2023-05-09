3f0d31d805226d3c6775a3429af09aae5c325f1f
adricastro1 <adricastro1196@gmail.com>
2023-05-09 15:20:50 -0400

    airtable and auth0 connections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import Airtable from 'airtable';
+import { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react'
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
+const base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base(process.env.REACT_APP_AIRTABLE_BASE_ID)
 
 function App() {
+
+  useEffect(() => {
+    base("stylists")
+    .select({view: "Grid view"})
+    .eachPage((records, fetchNextPage) => {
+      console.log(records);
+      fetchNextPage()
+    }
+    )
+  }
+  )
+
+  const { isLoading } = useAuth0()
+  if (isLoading) return <div>Loading...</div>
+
   return (
     <div className="App">
         <h1>HELLO</h1>
+        <LoginButton/>
+        <LogoutButton/>
+        <Profile/>
     </div>
   );
 }
