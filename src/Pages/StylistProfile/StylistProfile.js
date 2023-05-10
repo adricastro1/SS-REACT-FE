08b35969e61cdfa63001329b9b76ad089fa02033
adricastro1 <adricastro1196@gmail.com>
2023-05-09 20:00:34 -0400

    view stylist details

--- src/Pages/StylistProfile/StylistProfile.js
+++ src/Pages/StylistProfile/StylistProfile.js
@@ -1,12 +1,38 @@
+import Airtable from 'airtable';
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-function StylistProfile(props) {
-    const {id} = useParams()
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY }).base(process.env.REACT_APP_AIRTABLE_BASE_ID)
+
+
+const StylistProfile = (props) => {
+    const { id } = useParams()
     console.log("here is this stylist's:", id)
+    const [stylist, setStylist] = useState(null);
+
+    const getStylist = async () => {
+        try {
+          const record = await base("stylists").find(id);
+          setStylist(record);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      
+
+    useEffect(() => {
+        getStylist()
+    }, [id]);
+
+    if (!stylist) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <section>
-            <h1>Stylist Profile</h1>
+            <h1>{stylist.fields.Name}</h1>
+            <p>{stylist.fields.Bio}</p>
+            <p>{stylist.fields.Contact}</p>
         </section>
     )
 }
