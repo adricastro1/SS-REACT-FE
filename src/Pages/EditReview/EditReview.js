a84262260bcb8de1d86f89eef3727eab1dadb2e6
adricastro1 <adricastro1196@gmail.com>
2023-05-14 18:36:34 -0400

    initial edit form styling

--- src/Pages/EditReview/EditReview.js
+++ src/Pages/EditReview/EditReview.js
@@ -1,9 +1,10 @@
-import { Form, Button, Input, Rate } from 'rsuite';
 import 'rsuite/dist/rsuite-no-reset.min.css';
-import Airtable from "airtable";
+import './EditReview.css'
+import { Form, Button, Input, Rate, FlexboxGrid, Panel } from 'rsuite';
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import Airtable from "airtable";
 
 const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY }).base(
     process.env.REACT_APP_AIRTABLE_BASE_ID
@@ -55,19 +56,19 @@ const EditReviewForm = () => {
     };
 
     const fetchReviewData = async () => {
-      try {
-        const record = await base("Reviews").find(id);
-        setName(record.fields.Name);
-        setRating(record.fields.Rating);
-        setComment(record.fields.Comment);
-      } catch (error) {
-        console.error(error);
-      }
+        try {
+            const record = await base("Reviews").find(id);
+            setName(record.fields.Name);
+            setRating(record.fields.Rating);
+            setComment(record.fields.Comment);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     useEffect(() => {
         fetchReviewData();
-      }, [id]);
+    }, [id]);
 
 
     if (isLoading) {
@@ -76,25 +77,33 @@ const EditReviewForm = () => {
 
     return (
         isAuthenticated && (
-            <Form onSubmit={handleSubmit} className='Form'>
-            <h3 className='title'>Add a Review</h3>
-                <Form.Group controlId="name">
-                  <Form.ControlLabel>Name:</Form.ControlLabel>
-                  <Input type="text" value={name} onChange={(value) => setName(value)} />
-                </Form.Group>
-              
-                <Form.Group controlId="rating">
-                  <Form.ControlLabel>Rating:</Form.ControlLabel>
-                  <Rate value={rating} onChange={(value) => setRating(value)} />
-                </Form.Group>
-  
-            <Form.Group controlId="comment">
-              <Form.ControlLabel>Comment:</Form.ControlLabel>
-              <Input as="textarea" rows={3} value={comment} onChange={(value) => setComment(value)} />
-            </Form.Group>
-  
-            <Button appearance="primary" type="submit">Submit</Button>
-          </Form>
+            <Panel shaded className='Panel EditReview'>
+                <Form onSubmit={handleSubmit} className='Form'>
+                    <h3 className='title'>Add a Review</h3>
+                    <FlexboxGrid className='add-form'>
+                        <FlexboxGrid.Item colspan={10} className='add-name'>
+                            <Form.Group controlId="name">
+                                <Form.ControlLabel>Name:</Form.ControlLabel>
+                                <Input type="text" value={name} onChange={(value) => setName(value)} />
+                            </Form.Group>
+                        </FlexboxGrid.Item>
+
+                        <FlexboxGrid.Item colspan={8}>
+                            <Form.Group controlId="rating">
+                                <Form.ControlLabel>Rating:</Form.ControlLabel>
+                                <Rate value={rating} onChange={(value) => setRating(value)} />
+                            </Form.Group>
+                        </FlexboxGrid.Item>
+                    </FlexboxGrid>
+
+                    <Form.Group controlId="comment">
+                        <Form.ControlLabel>Comment:</Form.ControlLabel>
+                        <Input as="textarea" rows={3} value={comment} onChange={(value) => setComment(value)} />
+                    </Form.Group>
+
+                    <Button appearance="primary" type="submit">Submit</Button>
+                </Form>
+            </Panel>
         )
     );
 };
