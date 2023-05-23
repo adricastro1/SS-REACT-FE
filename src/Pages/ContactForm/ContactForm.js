69d8e27766a05fd4136be62ef48125f98d1ffb6d
adricastro1 <adricastro1196@gmail.com>
2023-05-23 19:25:30 -0400

    contact page mobile-responsive

--- src/Pages/ContactForm/ContactForm.js
+++ src/Pages/ContactForm/ContactForm.js
@@ -1,84 +1,56 @@
 import './ContactForm.css'
-import { Form, Button, Input, FlexboxGrid, Panel } from 'rsuite';
-import { useRef } from 'react';
+import { Panel } from 'rsuite';
+import { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
 
-export default function Mail() {
+function Mail() {
+  const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const USER_ID = process.env.REACT_APP_EMAILJS_USER_ID;
+
   const form = useRef();
+  const [isSuccess, setIsSuccess] = useState(false);
 
-  const sendEmail = async (e) => {
-    // e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-    const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-    const USER_ID = process.env.REACT_APP_EMAILJS_USER_ID;
-
-    try {
-      const result = await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID);
-      console.log(result.text);
-    } catch (error) {
-      console.log('Error sending email:', error);
-    }
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+        form.current.reset();
+        setIsSuccess(true);
+      }, (error) => {
+        console.log(error.text);
+      });
   };
 
-  const validateEmail = (email) => {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  };
 
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    const nameInput = form.current.elements['user_name'];
-    const emailInput = form.current.elements['user_email'];
-    const messageInput = form.current.elements['message'];
-    
-    if (!nameInput.value.trim()) {
-      alert('Please enter your name.');
-      return;
-    }
-
-    if (!emailInput.value.trim()) {
-      alert('Please enter your email address.');
-      return;
-    }
-
-    if (!validateEmail(emailInput.value)) {
-      alert('Please enter a valid email address.');
-      return;
-    }
-
-    if (!messageInput.value.trim()) {
-      alert('Please enter a message.');
-      return;
-    }
-
-    form.current.dispatchEvent(new Event('submit', { cancelable: true }));
-  };
-
-    return (
-        <Panel shaded className='Panel'>
-            <Form ref={form} onSubmit={handleSubmit} className='contact-form'>
-            <h2>Contact Us</h2>
-                <h5 className='contact-message'>Have questions or need more information? Contact us today! For consultation bookings, please include your preferred stylist's name (if any) and the date of your event if applicable. We're here to assist you with all your personal styling needs.</h5>
-                <FlexboxGrid>
-                    <FlexboxGrid.Item colspan={9}>
-                        <Form.ControlLabel>Name:</Form.ControlLabel>
-                        <Input type="text" name="user_name" required />
-                    </FlexboxGrid.Item>
-                    <FlexboxGrid.Item colspan={14} className='email'>
-                        <Form.ControlLabel>Email:</Form.ControlLabel>
-                        <Input type="email" name="user_email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
-                    </FlexboxGrid.Item>
-                </FlexboxGrid>
-                <Form.ControlLabel>Message:</Form.ControlLabel>
-                <Input as="textarea" rows={3} name="message" required></Input>
-                <Button 
-                appearance="primary" 
-                type="submit" 
-                value="Send"
-                className='contact-btn'
-                >Send</Button>
-            </Form>
-        </Panel>
-    );
+  return (
+    <Panel shaded className='Panel'>
+      <div>
+        <h2>Contact Us</h2>
+        <h5 className='contact-message'>Have questions or need more information? Contact us today! For consultation bookings, please include your preferred stylist's name (if any) and the date of your event if applicable. We're here to assist you with all your personal styling needs.</h5>
+        {isSuccess &&
+          <p className='sent'>Email sent successfully!</p>
+        }
+        <form ref={form} onSubmit={sendEmail}>
+          <div className='form-top'>
+            <div>
+              <label>Name:</label>
+              <input type="text" name="user_name" required />
+            </div>
+            <div>
+              <label>Email:</label>
+              <input type="email" name="user_email" required />
+            </div>
+          </div>
+          <label>Message:</label>
+          <textarea name="message" rows='4' required></textarea>
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </Panel>
+  );
 }
+
+export default Mail;
